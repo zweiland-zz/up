dd0fe595554a6dfb67e4bf2be9d8bad49a37da35
Zak Weiland <zweiland@gmail.com>
2017-06-21 16:54:26 -0500

    title filter fix

---
+++ src/common/components/Filters/components/Filters.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const { string } = PropTypes;
+const propTypes = {
+};
+
+function Filters(props) {
+  return (
+    <div>
+      Filters
+    </div>
+  );
+}
+
+Filters.propTypes = propTypes;
+export default Filters;
---
+++ src/common/components/Title/components/Title.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const { string } = PropTypes;
+const propTypes = {
+};
+
+function Title(props) {
+  return (
+    <div>
+      Title
+    </div>
+  );
+}
+
+Title.propTypes = propTypes;
+export default Title;
