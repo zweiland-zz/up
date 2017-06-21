d4c009ba02364045d73736994835560475a2112c
Zak Weiland <zweiland@gmail.com>
2017-06-21 08:11:14 -0500

    hello

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { } from 'react-bootstrap';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
-        <div className="app-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-
         <div className="bluehost" id="wrapper">
         <nav className="navbar navbar-inverse navbar-fixed-top">
         <div className="container-fluid">
