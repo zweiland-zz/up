f6b72688dead1af1fb7d7d60f9431f623de13cf7
Zak Weiland <zweiland@gmail.com>
2017-06-21 07:59:03 -0500

    sass

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
-import {  } from 'react-bootstrap';
-
-import './common/assets/styles/index.scss';
+import logo from './logo.svg';
 
 class App extends Component {
   render() {
     return (
       <div className="app">
+        <div className="app-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+
         <div className="bluehost" id="wrapper">
         <nav className="navbar navbar-inverse navbar-fixed-top">
         <div className="container-fluid">
