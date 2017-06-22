a1001cd8eda01e50006d71585ccd9d0ad232ec08
Zak Weiland <zweiland@gmail.com>
2017-06-22 12:14:16 -0500

    nav items

--- src/index.js
+++ src/index.js
@@ -11,6 +11,10 @@ import './styles/css/index.css';
 import App from './App';
 import AccountPage from './scenes/Account/AccountPage';
 import SitesPage from './scenes/Sites/SitesPage';
+import MarketplacePage from './scenes/Marketplace/MarketplacePage';
+import EmailPage from './scenes/Email/EmailPage';
+import DomainsPage from './scenes/Domains/DomainsPage';
+import AdvancedPage from './scenes/Advanced/AdvancedPage';
 
 import registerServiceWorker from './registerServiceWorker';
 
@@ -20,6 +24,10 @@ ReactDOM.render((
       <Route exact path="/" component={App}/>
       <Route exact path="/account" component={AccountPage}/>
       <Route exact path="/sites" component={SitesPage}/>
+      <Route exact path="/marketplace" component={MarketplacePage}/>
+      <Route exact path="/email" component={EmailPage}/>
+        <Route exact path="/domains" component={DomainsPage}/>
+      <Route exact path="/advanced" component={AdvancedPage}/>
     </div>
   </Router>
 ), document.getElementById('root'))
---
+++ src/scenes/Marketplace/MarketplacePage.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { } from 'react-router-dom'
+
+import Header from '../../components/Header/components/Header';
+import SidebarMenu from '../../components/SidebarMenu/components/SidebarMenu';
+
+class MarketplacePage extends Component {
+  render() {
+    return (
+      <div className="app">
+        <div id="wrapper">
+          <Header />
+          <SidebarMenu />
+          <div id="page-content-wrapper">
+            <div className="page-content">
+              <div className="container-fluid">
+                <div className="row">
+                  <div className="col-md-12">
+                    <h1>Marketplace</h1>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default MarketplacePage;
